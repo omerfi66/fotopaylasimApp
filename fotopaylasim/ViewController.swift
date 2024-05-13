b1957250c8995fcf2974cc71219f4c8a74972ca5
omer ipek <omer_furkan_ipek@hotmail.com>
2024-05-13 23:29:47 +0300

    Initial Commit

---
+++ fotopaylasim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fotopaylasim
+//
+//  Created by Ömer Furkan İpek on 13.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
