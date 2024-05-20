e34616ea366f767a8f0f7f669d74fa534536dfcc
omer ipek <omer_furkan_ipek@hotmail.com>
2024-05-20 17:44:13 +0300

    this is first commit

---
+++ fotopaylasim/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  fotopaylasim
+//
+//  Created by Ömer Furkan İpek on 19.05.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ post.swift
@@ -0,0 +1,8 @@
+//
+//  post.swift
+//  fotopaylasim
+//
+//  Created by Ömer Furkan İpek on 20.05.2024.
+//
+
+import Foundation
